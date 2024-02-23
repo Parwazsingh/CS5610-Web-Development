54aba5ed52fc6b9b27d9204c6f50402c200ffc99
Parwaz Singh Sarao <parwazekam@gmail.com>
2024-02-22 23:31:26 -0500

    commit 5

--- src/Labs/a3/Classes/index.tsx
+++ src/Labs/a3/Classes/index.tsx
@@ -4,7 +4,15 @@ function Classes() {
     const dangerous = true;
  return (
    <div>
+     
      <h2>Classes</h2>
+      <div className="wd-bg-yellow wd-fg-black wd-padding-10px">
+        Yellow background     </div>
+      <div className="wd-bg-blue wd-fg-black wd-padding-10px">
+        Blue background     </div>
+      <div className="wd-bg-red wd-fg-black wd-padding-10px">
+        Red background     </div>
+        <h2>Classes</h2>
      <div className={`${dangerous ? 'wd-bg-red' : 'wd-bg-green'}
                                      wd-fg-black wd-padding-10px`}>
        Dangerous background
