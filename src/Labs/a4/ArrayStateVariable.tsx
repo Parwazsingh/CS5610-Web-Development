12330b8b40f4954fab7ac93dc89089cbe4449ad8
Parwaz Singh Sarao <parwazekam@gmail.com>
2024-03-21 23:57:39 -0400

    commit 7

--- src/Labs/a4/ArrayStateVariable.tsx
+++ src/Labs/a4/ArrayStateVariable.tsx
@@ -10,12 +10,12 @@ function ArrayStateVariable() {
   return (
     <div>
       <h2>Array State Variable</h2>
-      <button onClick={addElement}>Add Element</button>
+      <button className = "btn btn-info" onClick={addElement}>Add Element</button>
       <ul>
         {array.map((item, index) => (
           <li key={index}>
             {item}
-            <button onClick={() => deleteElement(index)}>
+            <button className = "btn btn-danger"  onClick={() => deleteElement(index)}>
               Delete</button>
           </li>
         ))}
